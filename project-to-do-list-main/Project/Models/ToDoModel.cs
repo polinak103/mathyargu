97380c356b46627b19495cafead5c60c240aa291
Sonya3008 <s.vizgirda01@gmail.com>
2023-03-23 01:16:20 +0300

    hhh

---
+++ project-to-do-list-main/Project/Models/ToDoModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Models
+{
+    internal class ToDoModel
+    {
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+        private string _text;
+        private bool _isDone;
+        public bool IsDone
+        {
+            get { return _isDone; } 
+            set { _isDone = value; }
+        }
+        public string Text 
+        { 
+            get { return _text; } 
+            set { _text = value; } 
+        }
+    }
+}
